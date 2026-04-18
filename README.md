67a57b5e7e450cdd69a3540951364f0f2026bca0
akash-ind08 <akash.ald08@gmail.com>
2026-04-18 16:04:47 -0700

    Added cpython and other file

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# python-log-monitor
-Python-based log monitoring tool that detects errors and triggers alerts for system reliability and automation.
+ Python Log Monitoring & Alert System
+
+A lightweight Python tool that scans log files and detects critical errors like "ERROR" and "FAILED".
+
+## 🚀 Features
+- Detects error patterns in logs
+- Configurable keywords
+- Simple and fast execution
+- Useful for basic monitoring automation
+
+## 🛠 Tech Stack
+- Python
+- Linux (optional)
+
+## 📂 Project Structure
+python-log-monitor/
+│── monitor.py
+│── config.json
+│── sample.log
+│── requirements.txt
+
+## ▶️ How to Run
+1. Clone the repo
+2. Run:
+   python monitor.py
+
+## 📌 Use Case
+Helps developers and DevOps engineers quickly detect issues from logs and automate basic monitoring.
---
+++ monitor.py
@@ -0,0 +1,39 @@
+import json
+
+def load_config():
+    with open("config.json") as f:
+        return json.load(f)
+
+def check_logs(log_file, keywords):
+    with open(log_file, "r") as f:
+        lines = f.readlines()
+
+    alerts = []
+    for line in lines:
+        for keyword in keywords:
+            if keyword in line:
+                alerts.append(line.strip())
+
+    return alerts
+
+def send_alert(alerts):
+    print("\nALERTS DETECTED:")
+    for alert in alerts:
+        print(alert)
+
+def main():
+    config = load_config()
+    log_file = config["log_file"]
+    keywords = config["keywords"]
+
+    print("Monitoring logs...\n")
+
+    alerts = check_logs(log_file, keywords)
+
+    if alerts:
+        send_alert(alerts)
+    else:
+        print("No issues found")
+
+if _name_ == "_main_":
+    main()
